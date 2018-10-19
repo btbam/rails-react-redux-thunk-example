678403317c2abd5eae40da82f9e2380b9531ad1f
Dave Geffken <david.geffken@aig.com>
2018-10-19 00:47:02 -0400

    Add StyledElements component for centralized styled-components

---
+++ client/src/components/StyledElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  border: 1px solid black;
+  margin: 10px 0px;
+`;
+
+export const Th = styled.th`
+  border: 1px solid black;
+  padding: 5px;
+`;
+
+export const Td = styled.td`
+  border: 1px solid black;
+  padding: 5px;
+`;
+
+export const Button = styled.button`
+  margin: 10px 0px;
+`;
