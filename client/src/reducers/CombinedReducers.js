ecd4ca945afd51bdb176d20abf4f885a1a1140fe
Dave Geffken <david.geffken@aig.com>
2018-10-19 00:52:42 -0400

    Add People, PeopleEmailChars, and PeopleEmailDups to CombinedReducer

--- client/src/reducers/CombinedReducers.js
+++ client/src/reducers/CombinedReducers.js
@@ -2,10 +2,16 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
 import { me } from './Me'
+import people from './People'
+import peopleEmailChars from './PeopleEmailChars'
+import peopleEmailDups from './PeopleEmailDups'
 
 const Reducers = combineReducers({
   router: routerReducer,
   me,
+  people,
+  peopleEmailChars,
+  peopleEmailDups
 });
 
 export default Reducers
