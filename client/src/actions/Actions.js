778840146b627c5b1947da112c789b644f8cc5ef
Dave Geffken <david.geffken@aig.com>
2018-10-19 00:49:56 -0400

    Add actions for fetching people, processing email chars, and processing email dups

--- client/src/actions/Actions.js
+++ client/src/actions/Actions.js
@@ -6,3 +6,35 @@ export function receiveMe(me) {
     me
   }
 }
+
+export const fetchPeopleBegin = () => ({
+  type: actions.FETCH_PEOPLE_BEGIN
+});
+
+export const fetchPeopleSuccess = people => ({
+  type: actions.FETCH_PEOPLE_SUCCESS,
+  payload: { people }
+});
+
+export const fetchPeopleError = error => ({
+  type: actions.FETCH_PEOPLE_FAILURE,
+  payload: { error }
+});
+
+export const peopleEmailCharsBegin = () => ({
+  type: actions.PEOPLE_EMAIL_CHARS_BEGIN
+});
+
+export const peopleEmailCharsSuccess = chars => ({
+  type: actions.PEOPLE_EMAIL_CHARS_SUCCESS,
+  payload: { chars }
+});
+
+export const peopleEmailDupsBegin = () => ({
+  type: actions.PEOPLE_EMAIL_DUPS_BEGIN
+});
+
+export const peopleEmailDupsSuccess = dups => ({
+  type: actions.PEOPLE_EMAIL_DUPS_SUCCESS,
+  payload: { dups }
+});
